0e56e220fb05f9a94dd968d801bada5a70ea2c39
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-08 21:35:05 +0200

    document and export clearing cache

--- R/cache.R
+++ R/cache.R
@@ -1,11 +1,18 @@
+#' Clear cache
+#'
+#' @description Remove all \pkg{woodendesc}-related cached files.
+#'
+#' @return An integer code, invisibly, the same as in \code{\link[base]{unlink}}.
+#'
+#' @export
+wood_clear_cache <- function() {
+  unlink(file.path(wood_tempdir(), "*"))
+}
+
 wood_tempdir <- function() {
   dir_path <- file.path(tempdir(), "woodendesc")
   if (!dir.exists(dir_path)) {
     dir.create(dir_path)
   }
   dir_path
-}
-
-wood_clear_cache <- function() {
-  unlink(file.path(wood_tempdir(), "*"))
 }
