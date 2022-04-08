fee6f22a61d49d7ae6d9ef8d5fec535ad6ed32b7
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-08 21:29:02 +0200

    implement tests for wood_clear_cache()

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -7,3 +7,16 @@ test_that("wood_tempdir() always returns an existing directory", {
   # Running it the second time since the first call should create the directory
   expect_true(dir.exists(wood_tempdir()))
 })
+
+# CLEAR CACHE ----
+test_that("clearing cache doesn't remove woodendesc temporary directory", {
+  wood_clear_cache()
+  expect_true(dir.exists(file.path(tempdir(), "woodendesc")))
+})
+
+test_that("woodendesc temporary directory is empty after clearing cache", {
+  # Generate some cache data
+  file.create(file.path(wood_tempdir(), c("file1", "file2")))
+  wood_clear_cache()
+  expect_equal(length(list.files(wood_tempdir())), 0)
+})
