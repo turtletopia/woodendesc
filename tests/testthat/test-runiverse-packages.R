bcdd73a5086dabe9fc26434e8f1b97c4321f2726
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-09 21:07:58 +0200

    define behaviour for non-existent R universes

--- tests/testthat/test-runiverse-packages.R
+++ tests/testthat/test-runiverse-packages.R
@@ -22,3 +22,12 @@ test_that("tidyverse universe has ggplot2 package", {
     expect_true("ggplot2" %in% wood_runiverse_packages("tidyverse"))
   })
 })
+
+test_that("if a universe doesn't exist, returns an empty character vector with a warning", {
+  vcr::use_cassette("nonexistent-packages", {
+    expect_warning(
+      nonexistent_packages <- wood_runiverse_packages("nonexistent_universe")
+    )
+    expect_equal(nonexistent_packages, character())
+  })
+})
