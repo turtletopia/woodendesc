640c19a057623400a6647753774e9ef7e8bd23f7
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-09 10:16:36 +0200

    define tests for generating cache path

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -20,3 +20,25 @@ test_that("woodendesc temporary directory is empty after clearing cache", {
   wood_clear_cache()
   expect_equal(length(list.files(wood_tempdir())), 0)
 })
+
+# CACHE PATH ----
+test_that("cache path is an .rds file", {
+  expect_match(cache_path("path"), ".*\\.rds")
+})
+
+test_that("cache path separates name components with underscores", {
+  expect_match(cache_path("A", "B", "C"), ".*A_B_C\\..*")
+})
+
+test_that("cache file name begins with a \"cache_\"", {
+  expect_match(cache_path("say", "aloud"), ".*cache_say.*")
+})
+
+test_that("cache path is within woodendesc temporary directory", {
+  expect_match(cache_path("run", "Forest", "run"), wood_tempdir(), fixed = TRUE)
+})
+
+test_that("cache path depends on its arguments only (and tempdir() location)", {
+  file.create(cache_path("create", "a", "file"))
+  expect_true(file.exists(cache_path("create", "a", "file")))
+})
