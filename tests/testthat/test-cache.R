c132eaf1fd05658f5c15ecd9297848b9ea08f9e6
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-09 21:40:15 +0200

    change test to be independent of previous cache dir content

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -18,7 +18,8 @@ test_that("clearing cache removes only cache files from woodendesc directory", {
   # Generate some cache data
   file.create(file.path(wood_tempdir(), c("file1", "file2", "cache_A.txt")))
   wood_clear_cache()
-  expect_equal(list.files(wood_tempdir()), c("file1", "file2"))
+  expect_true(all(c("file1", "file2") %in% list.files(wood_tempdir())))
+  expect_false("cache_A.txt" %in% list.files(wood_tempdir()))
 })
 
 # CACHE PATH ----
