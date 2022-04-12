16bdf749e5fa2bf026c450343d3b188e599ea375
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-12 14:19:06 +0200

    add non-exported docs

--- R/utils-url.R
+++ R/utils-url.R
@@ -1,3 +1,15 @@
+#' Get R-universe URL
+#'
+#' @description Creates an URL to the selected universe and the selected API
+#' within this universe.
+#'
+#' @template universe
+#' @param ... \[\code{character(1)}\]\cr
+#'  Components of the URL path, to be separated by slashes.
+#'
+#' @return A single string with an URL address to selected universe.
+#'
+#' @noRd
 runiverse_url <- function(universe, ...) {
   paste(paste0("https://", universe, ".r-universe.dev"), ..., sep = "/")
 }
