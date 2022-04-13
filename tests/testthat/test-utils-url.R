62261a7c5a880fe0ece78439bc433e4061555193
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-13 17:11:22 +0200

    define tests for CRAN URL

--- tests/testthat/test-utils-url.R
+++ tests/testthat/test-utils-url.R
@@ -15,3 +15,24 @@ test_that("runiverse_url() separates API with slashes", {
     fixed = TRUE
   )
 })
+
+# CRAN URL ----
+test_that("cran_url() returns a single string", {
+  expect_vector(cran_url("-"),
+                ptype = character(),
+                size = 1)
+  expect_vector(cran_url("-", "allall"),
+                ptype = character(),
+                size = 1)
+  expect_vector(cran_url("-", "allall", params = list(start_key = "deepdep", limit = 1)),
+                ptype = character(),
+                size = 1)
+})
+
+test_that("cran_url() separates API with slashes", {
+  expect_match(
+    cran_url("-", "allall"),
+    "/-/allall",
+    fixed = TRUE
+  )
+})
