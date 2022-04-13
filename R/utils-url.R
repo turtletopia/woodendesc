fd477c5c727b44c1751b44da09a014da1d3c79f8
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-13 17:12:31 +0200

    implement CRAN URL

--- R/utils-url.R
+++ R/utils-url.R
@@ -12,3 +12,19 @@
 runiverse_url <- function(universe, ...) {
   paste(paste0("https://", universe, ".r-universe.dev"), ..., sep = "/")
 }
+
+cran_url <- function(..., params = list()) {
+  ret <- paste("http://crandb.r-pkg.org", ..., sep = "/")
+
+  # Add nothing if no parameters passed
+  if (length(params) == 0) return(ret)
+
+  # Wrap strings into quotation marks
+  params <- lapply(params, function(p) {
+    if (is.character(p)) paste0("\"", p, "\"") else p
+  })
+  # Translate list of parameters into URL list
+  params <- paste(names(params), params, sep = "=", collapse = "&")
+  # Add question mark before listing parameters
+  paste(ret, params, sep = "?")
+}
