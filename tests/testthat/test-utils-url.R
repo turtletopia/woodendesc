bd688cf71cab5bb086a7876c7e7d80d5b5b10408
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-13 18:12:05 +0200

    define tests for extracting core URL

--- tests/testthat/test-utils-url.R
+++ tests/testthat/test-utils-url.R
@@ -54,3 +54,24 @@ test_that("cran_url() separates API with slashes", {
     fixed = TRUE
   )
 })
+
+# EXTRACTING CORE URL ----
+test_that("extract_core_url() returns a single string", {
+  expect_vector(extract_core_url(runiverse_url("ropensci")),
+                ptype = character(),
+                size = 1)
+  expect_vector(extract_core_url(cran_url("-", "desc", params = list(limit = 1))),
+                ptype = character(),
+                size = 1)
+})
+
+test_that("core url has everything removed starting with a quotation mark", {
+  test_url <- extract_core_url("https://test.org?limit=10&start=11")
+  expect_no_match(test_url, "\\?.*")
+  expect_match(test_url, "https://test.org", fixed = TRUE)
+})
+
+test_that("if no parameters given, core url is the same as input", {
+  test_url <- "https://test.org"
+  expect_equal(extract_core_url(test_url), test_url)
+})
