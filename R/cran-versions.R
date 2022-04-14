e21e5da47875cbc674ffb543986811dfe9b6a2db
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-14 15:23:48 +0200

    add more caching with ver_latest()

--- R/cran-versions.R
+++ R/cran-versions.R
@@ -19,11 +19,18 @@ wood_cran_versions <- function(package) {
   cran_versions_cache(package)
 }
 
+#' @importFrom versionsort ver_latest
 cran_versions_cache <- function(package) {
   cache_file <- cache_path("versions", "CRAN", package)
 
   # Return usable cache if exists
   if (is_cache_usable(cache_file)) return(readRDS(cache_file))
+
+  # Return "expired" cache if no new version was published in the meantime
+  if (file.exists(cache_file)) {
+    versions <- readRDS(cache_file)
+    if (ver_latest(versions) == wood_cran_latest(package)) return(versions)
+  }
 
   # Download new data and cache it
   url <- cran_url("-", "allall", params = list(start_key = package, limit = 1))
