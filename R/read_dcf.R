fe91a29f87e1a3273f12e306812eda7b430719c6
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-15 20:30:58 +0200

    use regmatches() to simplify code

--- R/read_dcf.R
+++ R/read_dcf.R
@@ -1,6 +1,7 @@
 read_dcf_packages <- function(object) {
-  match <- gregexec("Package: ([^\n]+)\\n", object)[[1]]
-  start <- match[2, ]
-  end <- start + attr(match, "match.length", exact = TRUE)[2, ] - 1
-  unique(substring(object, start, end))
+  matches <- regmatches(
+    object,
+    gregexec("Package: (\\S+)", object)
+  )
+  unique(matches[[1]][2, ])
 }
