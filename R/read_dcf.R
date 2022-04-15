aca682ac3b0958db5be916a60170fb6fc662b1fe
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-15 21:39:54 +0200

    implement gregexec workaround for R < 4.1.0

--- R/read_dcf.R
+++ R/read_dcf.R
@@ -1,7 +1,16 @@
 read_dcf_packages <- function(object) {
-  matches <- regmatches(
-    object,
-    gregexec("Package: (\\S+)", object)
-  )
-  unique(matches[[1]][2, ])
+  if (R_older_than("4.1.0")) {
+    # gregexec was introduced in 4.1.0
+    matches <- regmatches(
+      object,
+      gregexpr("Package: (\\S+)", object)
+    )
+    unique(substring(matches[[1]], first = 10))
+  } else {
+    matches <- regmatches(
+      object,
+      gregexec("Package: (\\S+)", object)
+    )
+    unique(matches[[1]][2, ])
+  }
 }
