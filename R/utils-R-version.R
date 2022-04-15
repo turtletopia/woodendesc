608d177ad88268fba92eff3c773cb4a7b3b1edfd
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-15 21:31:31 +0200

    document an internal function

--- R/utils-R-version.R
+++ R/utils-R-version.R
@@ -1,3 +1,14 @@
+#' Check if R version is older than expected
+#'
+#' @description Check whether R version is older than expected, using comparison
+#' of version codes.
+#'
+#' @param \[\code{character(1)}\]\cr
+#'  A version code to compare to, e.g. "3.6.3"
+#'
+#' @return Logical vector of length one.
+#'
+#' @noRd
 #' @importFrom versionsort ver_latest
 R_older_than <- function(expected) {
   # TODO: implement with ver_older_than()
