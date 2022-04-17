a426247a9d38d4f108aacd069baa85fd26181751
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-17 19:40:42 +0200

    implement read_dcf()

--- R/read_dcf.R
+++ R/read_dcf.R
@@ -1,3 +1,20 @@
+read_dcf <- function(object) {
+  # Split into packages
+  packages <- strsplit(object, "\\n\\n")[[1]]
+  # Split into lines, each line being Key: Values
+  packages <- strsplit(packages, "\\n")
+  # Extract field names
+  packages <- lapply(packages, function(p_data) {
+    # Extract field names and field values as separate strings
+    split_data <- regmatches(p_data, regexpr(": ", p_data), invert = TRUE)
+    # Field names are first values in lists, values are second
+    ret <- lapply(split_data, `[[`, 2)
+    # Don't parse values until necessary
+    setNames(ret, vapply(split_data, `[[`, character(1), 1))
+  })
+  setNames(packages, vapply(packages, `[[`, character(1), "Package"))
+}
+
 read_dcf_packages <- function(object) {
   if (R_older_than("4.1.0")) {
     # gregexec was introduced in 4.1.0
