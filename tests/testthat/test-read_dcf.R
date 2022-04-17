1927987fe47d4ce5004709e8179f6ba6e3c74c8a
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-17 22:47:30 +0200

    define tests for newlines in read_dcf()

--- tests/testthat/test-read_dcf.R
+++ tests/testthat/test-read_dcf.R
@@ -4,11 +4,19 @@ PACKAGES_2 <- read_to_string("extdata", "misc_PACKAGES")
 
 # READ ALL ----
 cynkra_all <- read_dcf(PACKAGES)
+misc_all <- read_dcf(PACKAGES_2)
 
 test_that("read_dcf() returns a list of lists", {
   expect_vector(cynkra_all,
                 ptype = list())
   for (x in cynkra_all) {
+    expect_vector(x,
+                  ptype = list())
+  }
+
+  expect_vector(misc_all,
+                ptype = list())
+  for (x in misc_all) {
     expect_vector(x,
                   ptype = list())
   }
@@ -36,6 +44,29 @@ test_that("all package fields in read_dcf() are single strings", {
                     size = 1)
     }
   }
+})
+
+test_that("read_dcf() correctly parses multiline values when in the middle", {
+  expect_match(misc_all[["aaSEA"]][["Imports"]], "shinydashboard", fixed = TRUE)
+  expect_match(misc_all[["aaSEA"]][["Imports"]], "plotly", fixed = TRUE)
+})
+
+test_that("read_dcf() correctly parses multiline values when last field", {
+  expect_match(
+    misc_all[["CGIwithR"]][["URL"]],
+    "http://www.warwick.ac.uk/go/cgiwithr",
+    fixed = TRUE
+  )
+})
+
+test_that("read_dcf() preserves newlines in values", {
+  # Each of these fields contain exactly two newlines
+  expect_match(misc_all[["aaSEA"]][["Imports"]], "\\n.*\\n")
+  expect_match(misc_all[["CGIwithR"]][["URL"]], "\\n.*\\n")
+})
+
+test_that("read_dcf() doesn't introduce random newlines", {
+  expect_no_match(misc_all[["ABACUS"]][["Imports"]], "\\n")
 })
 
 # READ PACKAGES ----
