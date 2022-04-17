52cd8db85603217df9b014a0746dc3dc5917226b
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-18 00:00:35 +0200

    not use lookahead on Windows

--- R/read_dcf.R
+++ R/read_dcf.R
@@ -1,11 +1,19 @@
 read_dcf <- function(object) {
   # Split into packages
   packages <- strsplit(object, "\\n\\n")[[1]]
+
   # Split into lines, each line being Key: Values
   # Some fields are newlines
-  # Use lookahead to match and not strip Key characters
-  # As long as there are no Values with ":" in second or later line it will work
-  packages <- strsplit(packages, "\\n(?=[^\n]*: )", perl = TRUE)
+  if (.Platform$OS.type != "windows") {
+    # Use lookahead to match and not strip Key characters
+    # As long as there are no Values with ":" in second or later line it will work
+    packages <- strsplit(packages, "\\n(?=[^\n]*: )", perl = TRUE)
+  } else {
+    # Seems like lookahead may not work on Windows sometimes
+    packages <- gsub("\\n([^\n]+: )", "\n@\\1", packages)
+    packages <- strsplit(packages, "\n@")
+  }
+
   # Extract field names
   packages <- lapply(packages, function(p_data) {
     # Extract field names and field values as separate strings
