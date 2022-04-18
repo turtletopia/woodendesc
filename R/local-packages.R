eadb8ea7169af433700f771eb56a8ecb3efea06d
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-18 22:18:59 +0200

    write local_packages() docs

--- R/local-packages.R
+++ R/local-packages.R
@@ -1,3 +1,20 @@
+#' List locally available packages
+#'
+#' @description This function searches for installed packages inside a specified
+#' library path.
+#'
+#' @param path \[\code{character(1)}\]\cr
+#'  A path to local library, by default the first in `.libPaths()`.
+#'
+#' @return A character vector of available packages.
+#'
+#' @examples
+#' \donttest{
+#' wood_local_packages()
+#' }
+#'
+#' @family local
+#' @family packages
 #' @export
 wood_local_packages <- function(path = .libPaths()[1]) {
   list.dirs(path, full.names = FALSE, recursive = FALSE)
