8b546a67c11399e8221b5e98c498e65f7b884bcc
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-18 22:22:42 +0200

    add local to available families

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -3,6 +3,7 @@ list(
     runiverse = "Functions for R-universe:",
     cran = "Functions for CRAN:",
     url = "Functions for any URL:",
+    local = "Functions for local libraries:",
     packages = "Functions that query available packages:",
     versions = "Functions that query package versions:"
   )
