d9bb427bea0c3117d374072ad1150a05304d43c6
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-18 22:38:07 +0200

    test specifying multiple paths in local_packages()

--- tests/testthat/test-local-packages.R
+++ tests/testthat/test-local-packages.R
@@ -16,3 +16,22 @@ test_that("all returned strings are valid package names", {
 test_that("local packages include woodendesc package", {
   expect_subset("woodendesc", local_packages)
 })
+
+test_that("multiple paths may be specified", {
+  lib_dir <- tempdir()
+  withr::with_tempdir({
+    # Create fake local library
+    dir.create(file.path(lib_dir, "fakepackage"))
+    file.create(file.path(lib_dir, "fakepackage", "DESCRIPTION"))
+
+    # Call function
+    local_multiple <- wood_local_packages(c(.libPaths(), lib_dir))
+  }, tmpdir = lib_dir)
+
+  # Actual tests
+  expect_vector(local_multiple,
+                ptype = character())
+  expect_pkg_name(local_multiple)
+  expect_subset("woodendesc", local_multiple)
+  expect_subset("fakepackage", local_multiple)
+})
