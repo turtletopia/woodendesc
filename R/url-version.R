eaea70e116e84d588554e68723fb1cc946d9645f
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-18 18:40:33 +0200

    document url_version()

--- R/url-version.R
+++ R/url-version.R
@@ -1,3 +1,20 @@
+#' Get current package version in any repository
+#'
+#' @description This function queries any online repository for the code of the
+#' current version of the selected package.
+#'
+#' @template package
+#' @template repository
+#'
+#' @return A single string with a version code.
+#'
+#' @examples
+#' \donttest{
+#' wood_url_version("XML", repository = "http://www.omegahat.net/R")
+#' }
+#'
+#' @family url
+#' @family versions
 #' @export
 wood_url_version <- function(package, repository) {
   url_PACKAGES_cache(repository)[[package]][["Version"]]
