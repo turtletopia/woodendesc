ff45528f2d5b2cc7cf4afc97b78abc4fdd5af0b7
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-19 12:16:33 +0200

    remove superfluous test

--- tests/testthat/test-local-packages.R
+++ tests/testthat/test-local-packages.R
@@ -36,7 +36,6 @@ test_that("multiple paths may be specified", {
 
   expect_vector(local_multiple,
                 ptype = character())
-  expect_pkg_name(local_multiple)
   expect_subset(fake_lib_pkgs, local_multiple)
   expect_subset(local_packages, local_multiple)
 })
