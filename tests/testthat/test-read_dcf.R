77a62b6bd48848c8f1b2c6305eaa733d1715d8e5
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-19 20:51:32 +0200

    fix read_dcf_xxx tests

--- tests/testthat/test-read_dcf.R
+++ tests/testthat/test-read_dcf.R
@@ -85,14 +85,27 @@ test_that("read_dcf_all_values() returns non-empty string", {
   expect_gt(length(misc_packages), 0)
 })
 
-test_that("read_dcf_all_values() returns only valid package names", {
-  expect_pkg_name(cynkra_packages)
-  expect_pkg_name(misc_packages)
-})
-
-test_that("read_dcf_all_values() returns all expected packages from example file", {
+test_that("read_dcf_all_values() returns all expected values from example file", {
   expect_setequal(
     cynkra_packages,
     c("dm", "cynkrathis", "fledge", "indiedown", "munch", "tic", "tv")
   )
+})
+
+# READ ONE VALUE ----
+dm_version <- read_dcf_one_value(PACKAGES, "Version")
+A3_version <- read_dcf_one_value(PACKAGES_2, "Version")
+
+test_that("read_dcf_one_value() returns single string", {
+  expect_vector(dm_version,
+                ptype = character(),
+                size = 1)
+  expect_vector(A3_version,
+                ptype = character(),
+                size = 1)
+})
+
+test_that("read_dcf_one_value() returns an expected value from example file", {
+  expect_equal(dm_version, "0.2.8.9000")
+  expect_equal(A3_version, "1.0.0")
 })
