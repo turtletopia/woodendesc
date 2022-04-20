2be81a7f4de350b76caf5b466d258d11ec8d3040
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-20 16:42:53 +0200

    use built-in standard regexps

--- tests/testthat/setup-helpers.R
+++ tests/testthat/setup-helpers.R
@@ -1,12 +1,9 @@
 expect_pkg_name <- function(object) {
-  expect_match(
-    object, "^[^\\W\\d_]([^\\W_]|\\.)*[^\\W_]$", perl = TRUE
-  )
+  expect_match(object, .standard_regexps()$valid_package_name)
 }
 
 expect_version_code <- function(object) {
-  # Matches standard version codes with "major.minor.patch" naming schema
-  expect_match(object, "\\d+\\.\\d+\\.\\d+")
+  expect_match(object, .standard_regexps()$valid_numeric_version)
 }
 
 expect_subset <- function(object, expected) {
