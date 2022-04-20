dffb57e23c88cbc12f378b8a19217c417d761d91
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-20 17:29:36 +0200

    add 'dependencies' family

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -5,6 +5,7 @@ list(
     url = "Functions for any URL:",
     local = "Functions for local libraries:",
     packages = "Functions that query available packages:",
-    versions = "Functions that query package versions:"
+    versions = "Functions that query package versions:",
+    dependencies = "Functions that query package dependencies:"
   )
 )
