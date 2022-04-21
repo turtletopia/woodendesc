551c2c8c04cd7f8939ae51268119cd7b5f3db9a3
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-21 12:40:29 +0200

    implement local_dependencies()

---
+++ R/local-dependencies.R
@@ -0,0 +1,36 @@
+#' @export
+wood_local_dependencies <- function(package, paths = .libPaths()[1]) {
+  paths <- match_local_paths_args(paths)
+  # Find first path for which DESCRIPTION exists
+  desc_path <- Find(
+    file.exists, file.path(paths, package, "DESCRIPTION")
+  )
+  # If none found, Find() returns NULL
+  if (is.null(desc_path))
+    stop("package not found in the specified paths", call. = FALSE)
+
+  desc <- read_dcf(read_char(desc_path))[[package]]
+  deps <- lapply(
+    c("Depends", "Imports", "Suggests", "LinkingTo", "Enhances"),
+    function(dep_type) {
+      if (!is.null(desc[[dep_type]]))
+        cbind(parse_dependencies(desc[[dep_type]]),
+              type = dep_type)
+    }
+  )
+  do.call(rbind, deps)
+}
+
+parse_dependencies <- function(deps) {
+  deps <- strsplit(deps, ",")[[1]]
+  versions <- regmatches(deps, regexec(
+    "\\(>=\\s+(.*)\\)", deps
+  ))
+  versions <- vapply(versions, `[`, character(1), 2)
+  packages <- regmatches(deps, regexpr("\\S+", deps))
+  data.frame(
+    package = packages,
+    version = versions,
+    stringsAsFactors = FALSE
+  )
+}
