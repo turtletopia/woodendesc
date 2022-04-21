88208a54bee54550d64fe4cfd71ca55069774b59
Laura <erdaradun.gaztea+github@gmail.com>
2022-04-21 12:44:44 +0200

    document local_dependencies()

--- R/local-dependencies.R
+++ R/local-dependencies.R
@@ -1,3 +1,24 @@
+#' Get dependencies of locally available package
+#'
+#' @description This function extracts dependencies of the selected package,
+#' searching through specified library paths.
+#'
+#' @template package
+#' @template paths-local
+#'
+#' @return A data frame with three columns, all in string format:
+#' * `package` (package name),
+#' * `version` (minimum version requirement or `NA` if none),
+#' * `type` (dependency type, e.g. `"Imports"`).
+#'
+#' @examples
+#' \donttest{
+#' wood_local_dependencies("woodendesc")
+#' wood_local_dependencies("httr", paths = "all")
+#' }
+#'
+#' @family local
+#' @family dependencies
 #' @export
 wood_local_dependencies <- function(package, paths = .libPaths()[1]) {
   paths <- match_local_paths_args(paths)
