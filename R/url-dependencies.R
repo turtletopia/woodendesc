3ba7995f2b02c7b5a80f7cb741ffa5fbedf76778
Laura <erdaradun.gaztea+github@gmail.com>
2022-05-03 23:47:44 +0200

    document url_dependencies()

--- R/url-dependencies.R
+++ R/url-dependencies.R
@@ -1,4 +1,23 @@
-
+#' Get dependencies of a package in any repository
+#'
+#' @description This function queries any online repository for dependencies of
+#' the selected package.
+#'
+#' @template package
+#' @template repository
+#'
+#' @return A data frame with three columns, all in string format:
+#' * `package` (package name),
+#' * `version` (minimum version requirement or `NA` if none),
+#' * `type` (dependency type, e.g. `"Imports"`).
+#'
+#' @examples
+#' \donttest{
+#' wood_url_dependencies("XML", repository = "http://www.omegahat.net/R")
+#' }
+#'
+#' @family url
+#' @family dependencies
 #' @export
 wood_url_dependencies <- function(package, repository) {
   desc <- url_PACKAGES_cache(repository)[[package]]
