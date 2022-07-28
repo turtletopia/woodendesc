71f509ad06245d1bbbf5a6f9f68eb647d35c0dae
Laura <erdaradun.gaztea+github@gmail.com>
2022-07-28 17:17:59 +0200

    add tests for .if_null

--- tests/testthat/test-with_cache.R
+++ tests/testthat/test-with_cache.R
@@ -32,3 +32,26 @@ test_that("with_cache() works with outside variables", {
     100
   )
 })
+
+test_that("with_cache() returns NULL by default if value is NULL", {
+  expect_equal(
+    with_cache({ NULL }, "yet", "another", "cache"),
+    NULL
+  )
+})
+
+test_that("with_cache() doesn't create cache file if NULL", {
+  with_cache({ NULL }, "no", "cache")
+  expect_false(
+    cache_path("no", "cache") %in% list.files(wood_tempdir(), full.names = TRUE)
+  )
+})
+
+test_that("with_cache() accepts .if_null parameter that executes if NULL", {
+  expect_equal(
+    with_cache({}, "null", "cache", .if_null = {
+      NA_character_
+    }),
+    NA_character_
+  )
+})
