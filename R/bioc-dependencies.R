1665bf63c2358b6aff419f75714bd4b7fbd8cada
Laura <erdaradun.gaztea+github@gmail.com>
2022-07-28 21:01:22 +0200

    document bioc_dependencies()

--- R/bioc-dependencies.R
+++ R/bioc-dependencies.R
@@ -1,3 +1,29 @@
+#' Get dependencies of a package in Bioconductor
+#'
+#' @description This function queries Bioconductor for dependencies of the
+#' selected package for given Bioconductor release.
+#'
+#' @template package
+#' @template bioc-release
+#'
+#' @return A data frame with three columns, all in string format:
+#' * `package` (package name),
+#' * `version` (minimum version requirement or `NA` if none),
+#' * `type` (dependency type, e.g. `"Imports"`).
+#'
+#' @examples
+#' \donttest{
+#' wood_bioc_dependencies("Biostrings")
+#'
+#' # Will dependencies change?
+#' wood_bioc_dependencies("Biostrings", "devel")
+#' # And what about dependencies in the past?
+#' wood_bioc_dependencies("Biostrings", "2.10")
+#' wood_bioc_dependencies("Biostrings", "1.5")
+#' }
+#'
+#' @family bioc
+#' @family dependencies
 #' @export
 wood_bioc_dependencies <- function(package, release = "release") {
   desc <- bioc_PACKAGES_cache(release)[[package]]
