0b918bc864950189f515462d3d9c30c4c301b86f
Laura <erdaradun.gaztea+github@gmail.com>
2022-07-28 19:53:21 +0200

    document bioc_version()

--- R/bioc-version.R
+++ R/bioc-version.R
@@ -1,3 +1,25 @@
+#' Get available package version in Bioconductor
+#'
+#' @description This function queries Bioconductor for the code of the available
+#' version of the selected package for given Bioconductor release.
+#'
+#' @template package
+#' @template bioc-release
+#'
+#' @return A single string with a version code.
+#'
+#' @examples
+#' \donttest{
+#' wood_bioc_version("Biostrings")
+#'
+#' # What's coming next?
+#' wood_bioc_version("Biostrings", release = "devel")
+#' # Can query releases as old as 1.5:
+#' wood_bioc_version("Biostrings", release = "1.5")
+#' }
+#'
+#' @family bioc
+#' @family versions
 #' @export
 wood_bioc_version <- function(package, release = "release") {
   desc <- bioc_PACKAGES_cache(release)[[package]]
