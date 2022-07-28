c74c0fbae7174b98b9b4a1bca4b0a6f7a0e9666e
Laura <erdaradun.gaztea+github@gmail.com>
2022-07-28 17:33:57 +0200

    define tests for bioc_packages()

---
+++ tests/testthat/test-bioc-packages.R
@@ -0,0 +1,22 @@
+skip_if_not_installed("vcr")
+wood_clear_cache()
+
+# SETUP ----
+vcr::use_cassette("bioc-packages", {
+  bioc_packages <- wood_bioc_packages()
+})
+
+# TESTS ----
+test_packages(bioc_packages, wood_bioc_packages)
+
+test_that("there's a Biostrings package in the list", {
+  expect_subset("Biostrings", bioc_packages)
+})
+
+test_that("works with older releases", {
+  vcr::use_cassette("bioc-packages-old", {
+    bioc_packages_old <- wood_bioc_packages(release = "1.5")
+  })
+  expect_subset(c("affy", "Biostrings", "impute", "RMAGEML"), bioc_packages_old)
+  expect_length(bioc_packages_old, 97)
+})
