181e9a346aafdbaf129e430e7a1f7fc52eb7f73d
Laura <erdaradun.gaztea+github@gmail.com>
2022-07-31 22:39:37 +0200

    add core family to meta.R

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -2,6 +2,7 @@ list(
   rd_family_title = list(
     runiverse = "Functions for R-universe:",
     cran = "Functions for CRAN:",
+    core = "Functions for core R packages:",
     bioc = "Functions for Bioconductor:",
     url = "Functions for any URL:",
     local = "Functions for local libraries:",
