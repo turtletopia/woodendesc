5d8069855693e3f660cb34beecdceabbabe35b4d
Laura <erdaradun.gaztea+github@gmail.com>
2022-08-01 15:04:39 +0200

    document core_version()

--- R/core-version.R
+++ R/core-version.R
@@ -1,3 +1,19 @@
+#' Get current version of a core R package
+#'
+#' @description This function checks the version of the selected core R package.
+#'
+#' @template package
+#'
+#' @return A single string with a version code. Should be equal to the R version
+#' (\code{\link[base]{getRversion}()}).
+#'
+#' @examples
+#' \donttest{
+#' wood_core_version("graphics")
+#' }
+#'
+#' @family core
+#' @family versions
 #' @export
 wood_core_version <- function(package) {
   core_pkgs <- installed.packages(priority = "base")
