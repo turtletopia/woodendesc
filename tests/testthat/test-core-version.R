bfc12c0c35b8729e5b796097e489f4dcbeb503a5
Laura <erdaradun.gaztea+github@gmail.com>
2022-08-01 15:22:12 +0200

    simplify tests

--- tests/testthat/test-core-version.R
+++ tests/testthat/test-core-version.R
@@ -1,14 +1,6 @@
 utils_version <- wood_core_version("utils")
 
-test_that("returns a single string", {
-  expect_vector(utils_version,
-                ptype = character(),
-                size = 1)
-})
-
-test_that("returned string is a version code", {
-  expect_version_code(utils_version)
-})
+test_version(utils_version, wood_core_version, "utils", .test_cache = FALSE)
 
 test_that("raises an exception if package not available", {
   expect_error(wood_core_version("fakepackage"))
--- tests/testthat/test-local-packages.R
+++ tests/testthat/test-local-packages.R
@@ -6,18 +6,7 @@ lib_dir <- local_fake_library("fakepackage", "woodendesc")
 # TESTS ----
 local_packages <- wood_local_packages()
 
-test_that("returns a vector of strings", {
-  expect_vector(local_packages,
-                ptype = character())
-})
-
-test_that("returned vector of strings is not empty", {
-  expect_gt(length(local_packages), 0)
-})
-
-test_that("all returned strings are valid package names", {
-  expect_pkg_name(local_packages)
-})
+test_packages(local_packages, wood_local_packages, .test_cache = FALSE)
 
 test_that("local packages include woodendesc package", {
   expect_subset("woodendesc", local_packages)
