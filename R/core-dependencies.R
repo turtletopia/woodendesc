9cc05eb3e6af7f9aae11120700cd8d029ba688dc
Laura <erdaradun.gaztea+github@gmail.com>
2022-08-01 15:36:35 +0200

    document core_dependencies()

--- R/core-dependencies.R
+++ R/core-dependencies.R
@@ -1,3 +1,22 @@
+#' Get dependencies of a core R package
+#'
+#' @description This function collects the dependencies of the selected core R
+#' package.
+#'
+#' @template package
+#'
+#' @return A data frame with three columns, all in string format:
+#' * `package` (package name),
+#' * `version` (minimum version requirement or `NA` if none),
+#' * `type` (dependency type, e.g. `"Imports"`).
+#'
+#' @examples
+#' \donttest{
+#' wood_core_dependencies("tcltk")
+#' }
+#'
+#' @family core
+#' @family dependencies
 #' @export
 wood_core_dependencies <- function(package) {
   core_pkgs <- installed.packages(priority = "base")
