554665d7c96f79cea7e058fe378b8a377895c292
Laura <erdaradun.gaztea+github@gmail.com>
2022-08-02 22:40:30 +0200

    use safer URL as a test case

--- tests/testthat/test-wood-packages.R
+++ tests/testthat/test-wood-packages.R
@@ -6,14 +6,16 @@ vcr::use_cassette("lcb-packages", {
   packages <- wood_packages(repos = c("local#all", "cran", "bioc@1.5"))
 }, record = "new_episodes")
 
+vcr::use_cassette("wood-colinfay-packages", {
+  colinfay_packages <- wood_packages(repos = "https://colinfay.me")
+}, record = "new_episodes")
+
 # TESTS ----
 test_packages(packages)
 
-vcr::use_cassette("wood-omegahat-packages", {
-  test_that("uppercase in URL is preserved", {
-    expect_not_empty(wood_packages(repos = "http://www.omegahat.net/R"))
-  })
-}, record = "new_episodes")
+test_that("uppercase in URL is preserved", {
+  expect_not_empty(colinfay_packages)
+})
 
 test_that("empty vector is returned if repository doesn't exist", {
   expect_equal(
