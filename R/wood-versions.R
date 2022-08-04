619151b23429f0a51a19e472097cc7ee7a6782e1
Laura <erdaradun.gaztea+github@gmail.com>
2022-08-04 19:35:45 +0200

    document wood_versions()

--- R/wood-versions.R
+++ R/wood-versions.R
@@ -1,3 +1,26 @@
+#' Collect all package versions from multiple repositories
+#'
+#' @description This function queries the selected repositories for version
+#' codes of the selected package and collects the unique occurences.
+#'
+#' @template repos
+#'
+#' @return A character vector of version codes.
+#'
+#' @examples
+#' \donttest{
+#' wood_versions("woodendesc", c("local#all", "runiverse@turtletopia"))
+#' # Multiple packages are also possible:
+#' wood_versions(
+#'   c("ggplot2", "Biostrings", "woodendesc"),
+#'   repos = c("local#all", "cran", "bioc@1.5")
+#' )
+#' # By default, only CRAN is queried:
+#' wood_versions("versionsort")
+#' }
+#'
+#' @family wood
+#' @family versions
 #' @importFrom versionsort ver_sort
 #' @export
 wood_versions <- function(packages, repos = "cran") {
---
+++ man/roxygen/templates/packages.R
@@ -0,0 +1,2 @@
+#' @param packages \[\code{character()}\]\cr
+#'  Names of the packages to query for.
