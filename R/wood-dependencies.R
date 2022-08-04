b7f340e7012f5e8885f70841f22f2ed52bdbb551
Laura <erdaradun.gaztea+github@gmail.com>
2022-08-04 19:42:19 +0200

    document wood_dependencies()

--- R/wood-dependencies.R
+++ R/wood-dependencies.R
@@ -1,3 +1,31 @@
+#' Find dependencies from multiple repositories
+#'
+#' @description This function queries the selected repositories for dependencies
+#' of the selected packages and returns the first working occurence.
+#'
+#' @template packages
+#' @template repos
+#'
+#' @return A list named after queried packages, each element being a data frame
+#' with three columns, all in string format:
+#' * `package` (package name),
+#' * `version` (minimum version requirement or `NA` if none),
+#' * `type` (dependency type, e.g. `"Imports"`).
+#'
+#' @examples
+#' \donttest{
+#' wood_dependencies("stats", "core")
+#' # Multiple packages are also possible:
+#' wood_dependencies(
+#'   c("ggplot2", "Biostrings", "woodendesc"),
+#'   repos = c("runiverse@turtletopia", "cran", "bioc@1.5")
+#' )
+#' # By default, only CRAN is queried:
+#' wood_dependencies("versionsort")
+#' }
+#'
+#' @family wood
+#' @family dependencies
 #' @export
 wood_dependencies <- function(packages, repos = "cran") {
   query_makers <- interpret_repos(repos, context = "dependencies")
