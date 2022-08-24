7e15b857b46a8cb22579a29b6d9a1a3eba74d174
Laura <erdaradun.gaztea+github@gmail.com>
2022-08-24 22:31:58 +0200

    implement squash
    close #3

--- R/class-wood_dep_list.R
+++ R/class-wood_dep_list.R
@@ -9,3 +9,20 @@ as_wood_dep_list <- function(object) {
     class = c("wood_dep_list", class(object))
   )
 }
+
+#' @export
+squash <- function(object) {
+  UseMethod("squash")
+}
+
+#' @rdname squash
+#' @export
+squash.wood_dep_list <- function(object) {
+  # Filter out empty and NULL dependencies
+  deps <- object[!vapply(object, is.null, logical(1))]
+  deps <- deps[lengths(deps) > 0]
+  # Add origin column
+  deps <- mapply(cbind, origin = names(deps), deps, SIMPLIFY = FALSE)
+  # Merge data frames
+  do.call(rbind, c(deps, make.row.names = FALSE))
+}
