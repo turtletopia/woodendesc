2c6a94bf9fc536dd72ab41dbc09e7489769ea8e2
Laura <erdaradun.gaztea+github@gmail.com>
2022-08-25 22:57:06 +0200

    implement filter_dependencies()
    close #4

--- R/utils-dependencies.R
+++ R/utils-dependencies.R
@@ -32,3 +32,35 @@ squash.wood_dep_list <- function(object) {
   # Merge data frames
   do.call(rbind, c(deps, make.row.names = FALSE))
 }
+
+#' @export
+filter_dependencies <- function(object, which = "strong") {
+  UseMethod("filter_dependencies")
+}
+
+#' @rdname filter_dependencies
+#' @export
+filter_dependencies.wood_deps <- function(object, which = "strong") {
+  dep_types <- match_dep_type(which)
+  object[object[["type"]] %in% dep_types, ]
+}
+
+#' @rdname filter_dependencies
+#' @export
+filter_dependencies.wood_dep_list <- function(object, which = "strong") {
+  as_wood_dep_list(lapply(object, filter_dependencies, which = which))
+}
+
+match_dep_type <- function(which) {
+  dep_types <- c("Depends", "Imports", "LinkingTo", "Suggests", "Enhances")
+
+  if (identical(which, "all")) return(dep_types)
+  if (identical(which, "most")) return(dep_types[1:4])
+  if (identical(which, "strong")) return(dep_types[1:3])
+
+  # TODO: Make error messages more precise
+  if (!all(which %in% dep_types))
+    stop("Incorrect dependency type specified.", call. = FALSE)
+
+  which
+}
