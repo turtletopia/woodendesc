88fda13640af1cc4ac71250ce516ec4a757cc2b8
Laura <erdaradun.gaztea+github@gmail.com>
2022-08-28 11:25:40 +0200

    document github_packages()

--- R/github-packages.R
+++ R/github-packages.R
@@ -1,17 +1,39 @@
+#' List available package on a Github account
+#'
+#' @description This function finds packages among repositories belonging to a
+#' selected account. They are returned as a vector of strings, each element
+#' being a repository (and in most cases, package) name.
+#'
+#' @param user \code{character(1)}\cr
+#'  Name of a user or organization.
+#' @param include_forks \code{logical(1)}\cr
+#'  Whether to include packages forked from other accounts.
+#'
+#' @return A character vector of available packages.
+#'
+#' @examples
+#' \donttest{
+#' wood_github_packages("turtletopia")
+#' # I moved most my packages to turtletopia
+#' wood_github_packages("ErdaradunGaztea", include_forks = TRUE)
+#' }
+#'
+#' @family github
+#' @family packages
 #' @export
 wood_github_packages <- function(user, include_forks = FALSE) {
-  github_packages_cache(user, include_forks)
+  repos <- github_packages_cache(user)
+  if (!include_forks) {
+    repos <- Filter(Negate(is_fork), repos)
+  }
+  repos <- Filter(is_github_R_repo, repos)
+  vapply(repos, `[[`, character(1), "name")
 }
 
-github_packages_cache <- function(user, include_forks = FALSE) {
+github_packages_cache <- function(user) {
   with_cache({
     url <- github_url("users", user, "repos")
-    repos <- paginate(url)
-    if (!include_forks) {
-      repos <- Filter(Negate(is_fork), repos)
-    }
-    repos <- Filter(is_github_R_repo, repos)
-    vapply(repos, `[[`, character(1), "name")
+    paginate(url)
   }, "repos", "github", user)
 }
 
