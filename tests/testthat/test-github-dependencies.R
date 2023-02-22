7cff2fe1094fe2340020a4403f4568bab978c5e0
Laura <erdaradun.gaztea+github@gmail.com>
2023-02-22 11:27:12 +0100

    use additional cassette

--- tests/testthat/test-github-dependencies.R
+++ tests/testthat/test-github-dependencies.R
@@ -15,13 +15,15 @@ test_param_package(wood_github_dependencies, user = "turtletopia")
 test_param_gh_user(wood_github_dependencies, package = "gglgbtq")
 test_param_tag(wood_github_dependencies, package = "gglgbtq", user = "turtletopia")
 
-test_that("uses cache from wood_github_versions() if available if not latest commit", {
-  wood_clear_cache()
-  wood_github_versions("gglgbtq", "turtletopia")
-  expect_cache(wood_github_dependencies, gglgbtq_deps,
-               package = "gglgbtq",
-               user = "turtletopia",
-               tag = "v0.1.0")
+vcr::use_cassette("gglgbtq-deps-cache", {
+  test_that("uses cache from wood_github_versions() if available if not latest commit", {
+    wood_clear_cache()
+    wood_github_versions("gglgbtq", "turtletopia")
+    expect_cache(wood_github_dependencies, gglgbtq_deps,
+                 package = "gglgbtq",
+                 user = "turtletopia",
+                 tag = "v0.1.0")
+  })
 })
 
 vcr::use_cassette("gglgbtq-deps-latest", {
