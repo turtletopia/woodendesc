85a021ae63070d10f7763e0e9cf8909936bda780
Laura <erdaradun.gaztea+github@gmail.com>
2023-02-23 19:09:35 +0100

    test wood_dependencies()

--- tests/testthat/test-wood-dependencies.R
+++ tests/testthat/test-wood-dependencies.R
@@ -1,3 +1,17 @@
+skip_if_not_installed("vcr")
+wood_clear_cache()
+
+# SETUP ----
+pkgs <- c("versionsort", "gglgbtq", "woodendesc")
+vcr::use_cassette("mixed-deps", {
+  mixed_deps <- wood_dependencies(pkgs, repos = c("cran", "local#all"))
+})
+
 # TESTS ----
+test_list_of(mixed_deps, pkgs, test_dependencies)
 test_param_packages(wood_dependencies)
 test_param_repos(wood_dependencies, packages = c("versionsort", "gglgbtq"))
+
+test_that("returns a 'wood_dep_list' object", {
+  expect_s3_class(mixed_deps, "wood_dep_list")
+})
