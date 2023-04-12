6821e8a35341f2a1621161f7ad659b0a75834feb
Laura <erdaradun.gaztea+github@gmail.com>
2023-04-12 17:35:28 +0200

    fix and extend tests

--- tests/testthat/test-utils-query_maker.R
+++ tests/testthat/test-utils-query_maker.R
@@ -12,9 +12,23 @@ test_that("adding parameters updates the original object", {
   qm <- query_maker("runiverse", "packages")
   expect_null(qm[["args"]][["universe"]])
   # Is correctly added
-  qm <- add_parameter(qm, universe = "turtletopia")
+  qm <- add_parameter(qm, "turtletopia", "universe")
   expect_equal(qm[["args"]][["universe"]], "turtletopia")
   # The old parameter is not dropped when adding another
-  qm <- add_parameter(qm, x = 4)
+  qm <- add_parameter(qm, 4, "x")
   expect_equal(qm[["args"]], list(universe = "turtletopia", x = 4))
+})
+
+test_that("empty parameters are skipped when adding", {
+  qm <- query_maker("runiverse", "packages")
+  expect_equal(
+    add_parameter(qm, character(), "empty"),
+    qm
+  )
+})
+
+test_that("transformations are applied if specified", {
+  qm <- query_maker("runiverse", "packages")
+  qm <- add_parameter(qm, seq_len(10), "max", value_transform = max)
+  expect_equal(qm[["args"]][["max"]], 10)
 })
