d7561b5bd0f8a59f9bcf2aede2e78f9db8ffead4
Laura <erdaradun.gaztea+github@gmail.com>
2025-01-28 00:42:38 +0100

    start move to with_mock_api()
    This includes a stub CRAN response with first 15 packages only

--- tests/testthat/setup-httptest2.R
+++ tests/testthat/setup-httptest2.R
@@ -2,6 +2,8 @@ if (requireNamespace("httptest2", quietly = TRUE)) {
   library("httptest2")
 }
 
+httptest2::.mockPaths("../fx")
+
 with_mock_dir <- function(dir, expr) {
   httptest2::with_mock_dir(
     file.path("..", "fx", dir),
--- tests/testthat/test-wood-packages.R
+++ tests/testthat/test-wood-packages.R
@@ -2,7 +2,7 @@ skip_if_not_installed("httptest2")
 wood_clear_cache()
 
 # SETUP ----
-with_mock_dir("0", {
+httptest2::with_mock_api({
   packages <- wood_packages(repos = c("local#all", "cran", "bioc@1.8"))
 })
 
